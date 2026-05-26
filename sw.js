3e8158c9c66c293816feabcfd0d4594fc5bc4edc
mazhiang528 <1910142699@qq.com>
2026-05-26 10:30:12 +0800

    环球打卡 - 899个世界景点打卡应用

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'travel-v2';
+const ASSETS = ['/','/index.html','/data.js','/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok && e.request.url.startsWith('http')) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match(e.request)))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
